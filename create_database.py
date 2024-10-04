d8d85127a561f6b4ae674757fb7290683f7f017e
Yusu-f <yusouphishola01@gmail.com>
2024-10-04 12:43:23 +0100

    v1 done

--- create_database.py
+++ create_database.py
@@ -5,20 +5,20 @@ from langchain.schema import Document
 # from langchain.embeddings import OpenAIEmbeddings
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
-import openai 
+import openai
 from dotenv import load_dotenv
 import os
 import shutil
 
 # Load environment variables. Assumes that project contains .env file with API keys
 load_dotenv()
-#---- Set OpenAI API key 
-# Change environment variable name from "OPENAI_API_KEY" to the name given in 
+# ---- Set OpenAI API key
+# Change environment variable name from "OPENAI_API_KEY" to the name given in
 # your .env file.
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 CHROMA_PATH = "chroma"
-DATA_PATH = "data/books"
+DATA_PATH = "../docs-scaling/docs"
 
 
 def main():
@@ -32,14 +32,14 @@ def generate_data_store():
 
 
 def load_documents():
-    loader = DirectoryLoader(DATA_PATH, glob="*.md")
+    loader = DirectoryLoader(DATA_PATH, glob="*.md", recursive=True)
     documents = loader.load()
     return documents
 
 
 def split_text(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300,
+        chunk_size=600,
         chunk_overlap=100,
         length_function=len,
         add_start_index=True,
@@ -47,7 +47,7 @@ def split_text(documents: list[Document]):
     chunks = text_splitter.split_documents(documents)
     print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
 
-    document = chunks[10]
+    document = chunks[0]
     print(document.page_content)
     print(document.metadata)
 
--- query_data.py
+++ query_data.py
@@ -4,6 +4,16 @@ from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+import openai
+import os
+
+
+load_dotenv()
+# ---- Set OpenAI API key
+# Change environment variable name from "OPENAI_API_KEY" to the name given in
+# your .env file.
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 CHROMA_PATH = "chroma"
 
